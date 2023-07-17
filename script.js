d10096d74d0ca42e17754214452444a050227f78
euphoriiah-dev <euphoriiah.dev@gmail.com>
2023-07-17 18:55:10 +0200

    working on middle page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// SLIDESHOW SETTINGS
 let currentSlide = 0; // Initialize slider counter to 0
 
 function startSlider() {
@@ -22,3 +23,45 @@ if (currentSlide >= imageCount.length) {
 setInterval(() => {
   startSlider();
 }, 5000);
+
+// MIDDLE PAGE SCROLLING ANIMATIONS 
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
